9fde81c9ea0198b8767fa6e40ccdc2014710d409
vacip31 <burakatuk333@gmail.com>
2026-06-29 00:31:54 +0300

    feat: sinema & televizyon kelimeleri (1201-1230) eklendi

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* VibeTabu PWA Service Worker (sw.js) */
 
-const CACHE_NAME = 'vibetabu-cache-v33';
+const CACHE_NAME = 'vibetabu-cache-v34';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
