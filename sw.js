8d6568a0d9472b42eed3931dddf81511e49b2334
vacip31 <burakatuk333@gmail.com>
2026-06-29 00:33:37 +0300

    feat: spor & eglence oyunlari kelimeleri (1291-1320) eklendi

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* VibeTabu PWA Service Worker (sw.js) */
 
-const CACHE_NAME = 'vibetabu-cache-v36';
+const CACHE_NAME = 'vibetabu-cache-v37';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
