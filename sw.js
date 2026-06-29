fa24a94bf9b69e51f09e6fb6fd98517a02a85f44
vacip31 <burakatuk333@gmail.com>
2026-06-30 01:41:36 +0300

    style: enrich portal with multiple neon colors and title text gradients

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* VibeParty PWA Service Worker (sw.js) */
 
-const CACHE_NAME = 'vibeparty-cache-v35';
+const CACHE_NAME = 'vibeparty-cache-v36';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
