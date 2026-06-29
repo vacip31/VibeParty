0a56933c7b501f353894dcc2f4149a4144f7be8a
vacip31 <burakatuk333@gmail.com>
2026-06-30 01:37:57 +0300

    style: change portal screen color palette to custom layout

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* VibeParty PWA Service Worker (sw.js) */
 
-const CACHE_NAME = 'vibeparty-cache-v32';
+const CACHE_NAME = 'vibeparty-cache-v33';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
